7e3aa9347d438dc62590a97b37284f39e1fdf2df
Nicholas-Alves <41400491+Nicholas-Alves@users.noreply.github.com>
2021-03-07 13:02:37 -0300

    fix: AuthorDTO annotations, Pattern regex and Maven Dependencies for Bean Validations

--- src/main/java/com/nicholasalves/bookstoremanager/dto/AuthorDTO.java
+++ src/main/java/com/nicholasalves/bookstoremanager/dto/AuthorDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -22,6 +23,6 @@ public class AuthorDTO {
     private String name;
 
     @NotNull
-    @Size(max = 100)
+    @Max(value = 100)
     private Integer age;
 }
