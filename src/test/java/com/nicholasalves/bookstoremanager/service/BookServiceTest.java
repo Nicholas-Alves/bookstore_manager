057fbdafaab6334c58bc285a4b3329d5ac8ac32c
Nicholas-Alves <41400491+Nicholas-Alves@users.noreply.github.com>
2021-03-08 16:50:57 -0300

    add: Unit test for BookNotFoundException

--- src/test/java/com/nicholasalves/bookstoremanager/service/BookServiceTest.java
+++ src/test/java/com/nicholasalves/bookstoremanager/service/BookServiceTest.java
@@ -14,6 +14,8 @@ import java.util.Optional;
 
 import static com.nicholasalves.bookstoremanager.utils.BookUtils.createFakeBook;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -36,6 +38,16 @@ public class BookServiceTest {
         assertEquals(expectedFoundBook.getName(), bookDTO.getName());
         assertEquals(expectedFoundBook.getIsbn(), bookDTO.getIsbn());
         assertEquals(expectedFoundBook.getPublisherName(), bookDTO.getPublisherName());
+    }
+
+    @Test
+    void whenGivenUnexintingIdThenNotFindThrowAnException() {
+        var invalidId = 10L;
+
+        when(bookRepository.findById(invalidId))
+                .thenReturn(Optional.ofNullable(any(Book.class)));
+
+        assertThrows(BookNotFoundException.class, () -> bookService.findById(invalidId));
     }
 }
 
