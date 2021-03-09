c0035bc25250ce629b3f320ca112aa615ca38073
Nicholas-Alves <41400491+Nicholas-Alves@users.noreply.github.com>
2021-03-09 16:22:17 -0300

    add: Swagger-2 dependencies and SwaggerConfig class

--- src/main/java/com/nicholasalves/bookstoremanager/controller/BookController.java
+++ src/main/java/com/nicholasalves/bookstoremanager/controller/BookController.java
@@ -4,6 +4,8 @@ import com.nicholasalves.bookstoremanager.dto.BookDTO;
 import com.nicholasalves.bookstoremanager.dto.MessageResponseDTO;
 import com.nicholasalves.bookstoremanager.exception.BookNotFoundException;
 import com.nicholasalves.bookstoremanager.service.BookService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +13,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/books")
+@Api(value = "Livros")
 public class BookController {
 
     private final BookService bookService;
@@ -21,11 +24,16 @@ public class BookController {
     }
 
     @PostMapping
+    @ApiOperation(value = "Criação de livro",
+            notes = "Este endpoint recebe dados para criar um novo livro.")
     public MessageResponseDTO create(@RequestBody @Valid BookDTO bookDTO) {
         return bookService.create(bookDTO);
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Exibição de um livro",
+            notes = "Este endpoint recebe o ID de um livro " +
+            "e faz a busca na base dados, retornando os dados do livro e de seu autor.")
     public BookDTO findById(@PathVariable Long id) throws BookNotFoundException {
         return bookService.findById(id);
     }
